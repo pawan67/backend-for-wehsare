65cd3fc496e89f96888e2f1670a16d3c09cdbdaf
Pawan <pawantamada8@gmail.com>
2022-11-14 16:13:38 +0530

    changes

--- controllers/postControllers.js
+++ controllers/postControllers.js
@@ -8,7 +8,7 @@ const getAllPosts = asyncHandler(async (req, res) => {
 
 const createPost = asyncHandler(async (req, res) => {
   const { caption, image } = req.body;
-  
+
   const post = await Post.create({
     user: req.user._id,
     caption,
@@ -30,8 +30,51 @@ const deletePost = asyncHandler(async (req, res) => {
   }
 });
 
+const likePost = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (post) {
+    if (post.likes.includes(req.user._id)) {
+      await post.likes.pull(req.user._id);
+    } else {
+      await post.likes.push(req.user._id);
+    }
+    await post.save();
+    res.json(post);
+  } else {
+    res.status(404);
+    throw new Error("Post not found");
+  }
+});
+
+const getPostData = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (post) {
+    res.json(post);
+  } else {
+    res.status(404);
+    throw new Error("Post not found");
+  }
+});
+
+const getRelatedPosts = asyncHandler(async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  if (post) {
+    const posts = await Post.find({
+      _id: { $ne: post._id },
+      user: post.user,
+    }).sort({ createdAt: -1 });
+    res.json(posts);
+  } else {
+    res.status(404);
+    throw new Error("Post not found");
+  }
+});
+
 module.exports = {
   getAllPosts,
   createPost,
   deletePost,
+  likePost,
+  getPostData,
+  getRelatedPosts,
 };
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -3,11 +3,17 @@ const {
   getAllPosts,
   createPost,
   deletePost,
+  likePost,
+  getRelatedPosts,
+  getPostData,
 } = require("../controllers/postControllers");
 const { protect } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
 router.route("/").get(protect, getAllPosts).post(protect, createPost);
 router.route("/:id").delete(protect, deletePost);
+router.route("/like/:id").post(protect, likePost);
+router.route("/post/:id").get(getPostData);
+router.route("/relatedPosts/:id").get(getRelatedPosts);
 
 module.exports = router;
